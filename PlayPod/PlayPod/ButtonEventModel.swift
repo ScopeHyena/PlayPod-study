32f68d7ca67857105ff61e0a15038699257c0204
Scope.h <hongsch07@naver.com>
2024-06-30 12:08:30 +0900

    Added ButtonEvent Model
    ButtonREvent Model Added

---
+++ PlayPod/PlayPod/ButtonEventModel.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonEventModel.swift
+//  PlayPod
+//
+//  Created by Scope.H on 6/30/24.
+//
+
+import SwiftUI
+import Combine
+
+class ButtonEventTestModel: ObservableObject {
+    @Published var text: String = "Hello, World!"
+}
